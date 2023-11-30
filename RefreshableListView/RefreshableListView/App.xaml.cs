ce4aabc33f0a792123e5775043966375c5bf7f9e
DiwakarVenkatesan <diwakar.venkatesan@syncfusion.com>
2023-11-30 17:02:11 +0530

    Removed height and width codes in App.xaml.cs

--- RefreshableListView/RefreshableListView/App.xaml.cs
+++ RefreshableListView/RefreshableListView/App.xaml.cs
@@ -2,28 +2,11 @@
 {
     public partial class App : Application
     {
-        const int WindowWidth = 600;
-
-        const int WindowHeight = 800;
         public App()
         {
             InitializeComponent();
 
             MainPage = new MainPage();
-
-            Microsoft.Maui.Handlers.WindowHandler.Mapper.AppendToMapping(nameof(IWindow), (handler, view) =>
-            {
-#if WINDOWS
-                        var mauiWindow = handler.VirtualView;
-                var nativeWindow = handler.PlatformView;
-                nativeWindow.Activate();
-                IntPtr windowHandle = WinRT.Interop.WindowNative.GetWindowHandle(nativeWindow);
-                Microsoft.UI.WindowId windowId = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(windowHandle);
-                Microsoft.UI.Windowing.AppWindow appWindow = Microsoft.UI.Windowing.AppWindow.GetFromWindowId(windowId);
-                appWindow.Resize(new Windows.Graphics.SizeInt32(WindowWidth, WindowHeight));
-                nativeWindow.ExtendsContentIntoTitleBar = false;
-#endif
-            });
         }
     }
 }
